b4ec757f9e6e2558ca778430c5b764b7acd61345
Damian Godek <damian.godek1@gmail.com>
2018-11-19 18:15:48 +0100

    add server plugin

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,9 @@ module.exports = (env) => {
                 {
                     test: /\.js$/,
                     loader: "babel-loader"
+                    options: {
+                        plugins: env !== 'production' ? ['react-hot-loader/babel'] : []
+                    }
                 },
                 {
                     test: /\.css$/,
